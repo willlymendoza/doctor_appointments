ff321abb543d12eb54e471438dbe35f9a5750013
Willian <wyllmh5@gmail.com>
2020-07-06 16:34:27 -0600

    Added User Model

---
+++ models/UserModel.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 45,
+  },
+  last_name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 45,
+  },
+  phone_number: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 15,
+  },
+  address: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 25,
+  },
+  is_doctor: {
+    type: Boolean,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 20,
+    maxlength: 150,
+  },
+  created_by_id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  created_at: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  updated_at: {
+    type: Date,
+    required: true,
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
