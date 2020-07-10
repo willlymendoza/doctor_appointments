6a03e3b3c5ff8eb5f1e2ffbb17076927380fa306
Willian <wyllmh5@gmail.com>
2020-07-09 19:19:21 -0600

    Added validations to patientsController

--- routes/patientsController.js
+++ routes/patientsController.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Patient = require("../models/PatientModel");
+const { Types } = require("mongoose");
 
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
@@ -12,6 +13,9 @@ router.get("/", async (req, res) => {
 
 /* SEARCH PATIENT BY ID */
 router.get("/:id", async (req, res) => {
+  const validId = Types.ObjectId.isValid(req.params.id);
+  if (!validId) return res.status(400).send("The ID param is invalid");
+
   const patient = await Patient.findById(req.params.id);
 
   if (!patient) return res.status(404).send("Patient not found");
@@ -20,51 +24,98 @@ router.get("/:id", async (req, res) => {
 });
 
 /* CREATE NEW PATIENT */
-router.post("/", async (req, res) => {
-  const patient = new Patient();
+router.post(
+  "/",
+  [
+    body("first_name").trim().isLength({ min: 5, max: 55 }),
+    body("last_name").trim().isLength({ min: 5, max: 55 }),
+    body("personal_document_id").trim().isLength({ min: 5, max: 55 }),
+    body("phone_number").trim().isLength({ min: 8, max: 25 }),
+    body("email").trim().isEmail(),
+    body("city").trim().isLength({ min: 3, max: 40 }),
+    body("address").trim().isLength({ min: 5, max: 60 }),
+    body("sex").trim().isLength({ min: 1, max: 3 }),
+    body("age").trim().isInt({ min: 1, max: 150 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  patient.first_name = req.body.first_name;
-  patient.last_name = req.body.last_name;
-  patient.personal_document_id = req.body.personal_document_id;
-  patient.phone_number = req.body.phone_number;
-  patient.email = req.body.email;
-  patient.city = req.body.city;
-  patient.address = req.body.address;
-  patient.sex = req.body.sex;
-  patient.age = req.body.age;
-  /* CHANGE THIS WHEN APLIYING USERS AUTH */
-  patient.created_by_id = "5f0149db7c41292dec984f82";
-  patient.updated_at = Date.now();
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+      });
+    }
 
-  await patient.save();
+    const patient = new Patient();
 
-  res.status(201).send(patient);
-});
+    patient.first_name = req.body.first_name;
+    patient.last_name = req.body.last_name;
+    patient.personal_document_id = req.body.personal_document_id;
+    patient.phone_number = req.body.phone_number;
+    patient.email = req.body.email;
+    patient.city = req.body.city;
+    patient.address = req.body.address;
+    patient.sex = req.body.sex;
+    patient.age = req.body.age;
+    /* CHANGE THIS WHEN APLIYING USERS AUTH */
+    patient.created_by_id = "5f0149db7c41292dec984f82";
+    patient.updated_at = Date.now();
+
+    await patient.save();
+
+    res.status(201).send(patient);
+  }
+);
 
 /* UPDATING PATIENT */
-router.put("/:id", async (req, res) => {
-  const patient = await Patient.findByIdAndUpdate(
-    req.params.id,
-    {
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      personal_document_id: req.body.personal_document_id,
-      phone_number: req.body.phone_number,
-      email: req.body.email,
-      city: req.body.city,
-      address: req.body.address,
-      sex: req.body.sex,
-      age: req.body.age,
-      updated_at: Date.now(),
-    },
-    {
-      new: true,
+router.put(
+  "/:id",
+  [
+    body("first_name").trim().isLength({ min: 5, max: 55 }),
+    body("last_name").trim().isLength({ min: 5, max: 55 }),
+    body("personal_document_id").trim().isLength({ min: 5, max: 55 }),
+    body("phone_number").trim().isLength({ min: 8, max: 25 }),
+    body("email").trim().isEmail(),
+    body("city").trim().isLength({ min: 3, max: 40 }),
+    body("address").trim().isLength({ min: 5, max: 60 }),
+    body("sex").trim().isLength({ min: 1, max: 3 }),
+    body("age").trim().isInt({ min: 1, max: 150 }),
+  ],
+  async (req, res) => {
+    const validId = Types.ObjectId.isValid(req.params.id);
+    if (!validId) return res.status(400).send("The ID param is invalid");
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+      });
     }
-  );
 
-  if (!patient) return res.status(404).send(`This patient doesn't exist`);
+    const patient = await Patient.findByIdAndUpdate(
+      req.params.id,
+      {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        personal_document_id: req.body.personal_document_id,
+        phone_number: req.body.phone_number,
+        email: req.body.email,
+        city: req.body.city,
+        address: req.body.address,
+        sex: req.body.sex,
+        age: req.body.age,
+        updated_at: Date.now(),
+      },
+      {
+        new: true,
+      }
+    );
 
-  res.status(200).send(patient);
-});
+    if (!patient) return res.status(404).send(`This patient doesn't exist`);
+
+    res.status(200).send(patient);
+  }
+);
 
 module.exports = router;
