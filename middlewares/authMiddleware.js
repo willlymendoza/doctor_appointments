3eaad178214505fc0243a464bce43fd50c328f75
Willian <wyllmh5@gmail.com>
2020-07-10 18:33:06 -0600

    Added authMiddleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+  const jwtToken = req.header("Authorization");
+
+  if (!jwtToken)
+    return res.status(401).send("Access denied, valid token needed");
+
+  try {
+    const payload = jwt.verify(jwtToken, "yuyujuju");
+    req.user = payload;
+
+    next();
+  } catch (error) {
+    res.status(400).send("Access denied, Invalid Token");
+  }
+}
+
+module.exports = auth;
