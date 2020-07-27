8e753426ea576b7c61bcc96144b5aa56682ff229
Willian <wyllmh5@gmail.com>
2020-07-27 13:18:59 -0600

    Added ymdDateFromFormat & ymdDateToFormat functions

--- helpers/dateFormat.js
+++ helpers/dateFormat.js
@@ -5,8 +5,18 @@ const postDateFormat = (date) => {
 };
 
 const ymdDateFormat = (date) => {
-  return moment(date).format("yyyy-MM-DD");
+  return moment(date).format("yyyy-DD-MM");
+};
+
+const ymdDateFromFormat = (date) => {
+  return moment(date).set({ hour: 0, second: 0, minute: 0 }).format();
+};
+
+const ymdDateToFormat = (date) => {
+  return moment(date).set({ hour: 23, second: 59, minute: 59 }).format();
 };
 
 module.exports.postDateFormat = postDateFormat;
 module.exports.ymdDateFormat = ymdDateFormat;
+module.exports.ymdDateFromFormat = ymdDateFromFormat;
+module.exports.ymdDateToFormat = ymdDateToFormat;
