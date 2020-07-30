c41806805335f5c5b639bee4f15b03c4538c677a
Willian <wyllmh5@gmail.com>
2020-07-29 22:55:13 -0600

    Deleted postDateFormat function

--- helpers/dateFormat.js
+++ helpers/dateFormat.js
@@ -1,9 +1,5 @@
 const moment = require("moment");
 
-const postDateFormat = (date) => {
-  return moment(date).format();
-};
-
 const ymdDateFormat = (date) => {
   return moment(date).format("yyyy-DD-MM");
 };
@@ -16,7 +12,6 @@ const ymdDateToFormat = (date) => {
   return moment(date).set({ hour: 23, second: 59, minute: 59 }).format();
 };
 
-module.exports.postDateFormat = postDateFormat;
 module.exports.ymdDateFormat = ymdDateFormat;
 module.exports.ymdDateFromFormat = ymdDateFromFormat;
 module.exports.ymdDateToFormat = ymdDateToFormat;
